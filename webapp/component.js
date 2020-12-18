68dba14240789e19b75997086b9ed5c2d9d3891f
JiJSaS <nikand8121@gmail.com>
2020-12-18 16:39:32 +0300

    Step 10: Descriptor
    Adjusted manifest and fixed some previous problems

--- webapp/component.js
+++ webapp/component.js
@@ -1,17 +1,11 @@
 sap.ui.define([
     "sap/ui/core/UIComponent", 
     "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
 ], function(UIComponent, JSONModel, ResourceModel){
     "use strict";
-    return UIComponent.extend("sap.ui.walkthrough.Component", {
+    return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
         metadata: {
-            rootView: {
-                "viewName": "sap.ui.demo.walkthrough.view.App",
-                "type": "XML",
-                "async": true,
-                "id":"app"
-            }
+            manifest:"json"
         },
         init: function(){
             //call init of parent UI from SAP
@@ -24,14 +18,6 @@ sap.ui.define([
              };
             var oModel = new JSONModel(oData);
             this.setModel(oModel);
-
-                //set i18n model on view
-            var i18nModel = new ResourceModel({
-            bundleName:"sap.ui.demo.walkthrough.i18n.i18n",
-            supportedLocales: [""],
-            fallbackLocale: ""
-            });
-            this.setModel(i18nModel, "i18n");
         }
     });
 })
