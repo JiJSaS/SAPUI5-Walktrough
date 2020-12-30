961ea3a683e6b21e504307387e1a1e814d6f5876
JiJSaS <nikand8121@gmail.com>
2020-12-30 23:31:47 +0300

    Step 16: Dialogs and Fragments
    Added new button which calls dialogue fragment
    pretty cool stuff

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function(Controller, MessageToast){
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
+], function(Controller, MessageToast, Fragment){
     'use strict';
     return Controller.extend("sap.ui.walkthrough.controller.HelloPanel", {
         onShowHello : function () {
@@ -9,9 +10,24 @@ sap.ui.define([
             var oBundle = this.getView().getModel("i18n").getResourceBundle();
             var sRecipient = this.getView().getModel().getProperty("/recipient/name");
             var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-    
             // show message
             MessageToast.show(sMsg);
+        },
+        onOpenDialogue: function(){
+            var oView = this.getView();
+            //create dialog lazily
+            if (!this.pDialog){
+                //load it async XML fragment
+                this.pDialog = Fragment.load({
+                    id: oView.getId(),
+                    name: "sap.ui.demo.walkthrough.view.HelloDialogue"
+                }).then(function(oDialog){
+                    //conntect dialog to the root view of component (models + lifecycle)
+                    oView.addDependent(oDialog);
+                    return oDialog;
+                });
+            }
+            this.pDialog.then(function(oDialog){ oDialog.open();});
         }
     });
     
